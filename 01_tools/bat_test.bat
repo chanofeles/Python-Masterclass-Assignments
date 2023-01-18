7a041e93026dbd7d629989eab12bf9bef2c871ab
Keyframe <chanofeles@hotmail.com>
2023-01-18 11:20:32 -0500

    DEL error lines

--- 01_tools/bat_test.bat
+++ 01_tools/bat_test.bat
@@ -3,8 +3,5 @@
 set NUKE_VERSION=Nuke13.2
 set NUKE_PATH=C:\Program Files\%NUKE_VERSION%v4
 
-:: --- SET PYTHON SCRIPT PATH ---
-set PYTHON_PATH=E:\PythonMasterclass\assignments\01_tools
-
 :: --- START NUKE WITH FLAGS FOR NUKE X AND PAUSED VIEWER TO AVOID CRASHING ON HEAVY SCRIPTS---
 "%NUKE_PATH%\%NUKE_VERSION%.exe" --nukex --pause
