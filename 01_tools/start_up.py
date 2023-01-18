666a0540675780b795d26600eb0f0b24739e5a63
Keyframe <chanofeles@hotmail.com>
2023-01-18 11:18:22 -0500

    ADDING flags to nuke start command

---
+++ 01_tools/start_up.py
@@ -0,0 +1,13 @@
+#******************************************************************************
+# content         = Nuke Start up message
+#
+# creation date   = 13/01/2023 
+#
+# description     = Nuke start up message 
+#
+# author          = Fernando Arbelaez <fernandoa@iknowvfx.com>
+# 
+#******************************************************************************
+
+## DEFINE MESSAGE ##
+startup_msg = 'We have successfully launched nuke via batch'
