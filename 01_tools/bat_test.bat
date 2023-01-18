44e1a6f418648a686c58d1348d54cea089961a66
Keyframe <chanofeles@hotmail.com>
2023-01-18 10:35:01 -0500

    ADD bat assignments

---
+++ 01_tools/bat_test.bat
@@ -0,0 +1,8 @@
+@echo off
+:: --- NUKE VERSION ---
+set NUKE_VERSION=Nuke13.2
+set NUKE_PATH=C:\Program Files\%NUKE_VERSION%v4
+
+:: --- START NUKE---
+"%NUKE_PATH%\%NUKE_VERSION%.exe" --nukex
+
---
+++ 01_tools/shell_assignment md.bat
@@ -0,0 +1,39 @@
+#******************************************************************************
+# content         = Shell commands
+#
+# creation date   = 18/01/2023 
+#
+# description     = Advanced tools Shell assignment No.1
+#
+# author          = Fernando Arbelaez <fernandoa@iknowvfx.com>
+# 
+#******************************************************************************
+
+:: a) Create the directory "shell_test" (and changing directory)
+cd C:\Users\Keyframe\Desktop
+md 01_assignment_shell
+cd 01_assignment_shell
+
+:: b) Create the file "shell_print.py" with a simple print into the directory
+echo print('This will get difficult for sure!') > shell_print.py
+
+:: c) Rename the file to "new_shell_print.py"
+ren shell_print.py new_shell_print.py
+
+:: d) List what is in the directory "shell_test" including their file permissions
+dir 
+icacls new_shell_print.py
+
+:: e) Execute the Python file and call the simple print
+python new_shell_print.py
+
+:: f) Remove the directory "shell_test" with its content
+cd ..
+rmdir /s 01_assignment_shell
+
+
+
+
+
+
+cd C:\Program Files\Nuke13.0v1\
